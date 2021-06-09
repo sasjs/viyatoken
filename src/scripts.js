17c92086e93e5b6b89267702d73d6e313f5a4dbd
sabhas <sabirbk06@gmail.com>
2021-06-09 15:52:02 +0500

    fix #10: auto approve checked by default and now working

--- src/scripts.js
+++ src/scripts.js
@@ -188,7 +188,7 @@ async function generateToken() {
 
         if (!res.error) {
           clientSettingsContainer.style.display = 'none';
-          goToAuthPage();
+          goToAuthPage(clientSettings.autoapprove);
         } else {
           resetGenerateScreen();
         }
@@ -205,102 +205,121 @@ function resetGenerateScreen() {
   clientSettingsContainer.style.display = '';
 }
 
-function goToAuthPage() {
-    let authUrl = `/SASLogon/oauth/authorize?client_id=${client}&response_type=code`;
+function goToAuthPage(autoApprove) {
+  let authUrl = `/SASLogon/oauth/authorize?client_id=${client}&response_type=code`;
 
-    fetch(location.origin + authUrl)
-    .then(response => response.text())
-    .then(response => {
-        let authorizeUrl = location.origin + '/SASLogon/oauth/authorize';
-        let params = {};
+  fetch(location.origin + authUrl)
+  .then(response => response.text())
+  .then(response => {
+    let authorizeUrl = location.origin + '/SASLogon/oauth/authorize';
+    let params = {};
+    let responseBody = response.split("<body>")[1].split("</body>")[0];
+    let bodyElement = document.createElement("div");
+    if (autoApprove === 'true') {
+      bodyElement.innerHTML = responseBody;
+      authcode = bodyElement.querySelector('.infobox h4').innerText;
 
-        let responseBody = response.split("<body>")[1].split("</body>")[0];
-        let bodyElement = document.createElement("div");
-        bodyElement.innerHTML = responseBody;
+      let data = {
+          'fromjs': [
+              {
+                  client: client,
+                  secret: secret,
+                  authcode: authcode
+              }
+          ]
+      }
+      tokenAuth(data)
+    }
+    else {
+      bodyElement.innerHTML = responseBody;
+      let form = bodyElement.querySelector("#application_authorization");
 
-        let form = bodyElement.querySelector("#application_authorization");
+      if (!form) {
+        resetGenerateScreen();
+        return;
+      }
 
-        if (!form) {
-          resetGenerateScreen();
-          return;
+      let inputs = form.querySelectorAll("input");
+
+      for (let input of inputs) {
+        if (input.name === "user_oauth_approval") {
+          input.value = "true";
         }
 
-        let inputs = form.querySelectorAll("input");
+        params[input.name] = input.value;
+      }
 
-        for (let input of inputs) {
-          if (input.name === "user_oauth_approval") {
-            input.value = "true";
+      let formData = new FormData();
+
+      for (const key in params) {
+        if (params.hasOwnProperty(key)) {
+          formData.append(key, params[key]);
+        }
+      }
+
+      fetch(authorizeUrl, {
+          method: "POST",
+          credentials: "include",
+          body: formData,
+          referrerPolicy: "same-origin"
+      }).then(response => response.text())
+      .then(response => {
+          let responseBody = response.split("<body>")[1].split("</body>")[0];
+          let bodyElement = document.createElement("div");
+          bodyElement.innerHTML = responseBody;
+
+          authcode = bodyElement.querySelector('.infobox h4').innerText;
+
+          let data = {
+              'fromjs': [
+                  {
+                      client: client,
+                      secret: secret,
+                      authcode: authcode
+                  }
+              ]
           }
+          tokenAuth(data)
+      });
+    }
+  });
+}
 
-          params[input.name] = input.value;
-        }
+function tokenAuth(data) {
+  sasJs.request('services/common/tokenauth', data).then(res => {
+    const generateTokenButton = document.querySelector('#generate-token');
+    generateTokenButton.style.display = 'none';
 
-        let formData = new FormData();
+    let tokenInfo = JSON.stringify(res.tokeninfo[0], null, 2)
+      .replace("ACCESS_TOKEN", "access_token")
+      .replace("CLIENT", "client")
+      .replace("SECRET", "secret")
+      .replace("AUTHCODE", "authcode")
+      .replace("REFRESH_TOKEN", "refresh_token");
 
-        for (const key in params) {
-          if (params.hasOwnProperty(key)) {
-            formData.append(key, params[key]);
-          }
-        }
+    let tokenInfoEnv = `CLIENT=${res.tokeninfo[0].CLIENT}\nSECRET=${res.tokeninfo[0].SECRET}\nACCESS_TOKEN=${res.tokeninfo[0].ACCESS_TOKEN}\nREFRESH_TOKEN=${res.tokeninfo[0].REFRESH_TOKEN}`;
 
-        fetch(authorizeUrl, {
-            method: "POST",
-            credentials: "include",
-            body: formData,
-            referrerPolicy: "same-origin"
-        }).then(response => response.text())
-        .then(response => {
-            let responseBody = response.split("<body>")[1].split("</body>")[0];
-            let bodyElement = document.createElement("div");
-            bodyElement.innerHTML = responseBody;
+    let access_token_macro = chunkString(res.tokeninfo[0].ACCESS_TOKEN, 240).join('%trim(\r\n)');
+    let refresh_token_macro = chunkString(res.tokeninfo[0].REFRESH_TOKEN, 240).join('%trim(\r\n)');
 
-            authcode = bodyElement.querySelector('.infobox h4').innerText;
+    let tokenInfoMacro = `%let client=${res.tokeninfo[0].CLIENT};<br/>%let secret=${res.tokeninfo[0].SECRET};<br/>%let access_token=${access_token_macro};<br/>%let refresh_token=${refresh_token_macro};<br/>`;
 
-            let data = {
-                'fromjs': [
-                    {
-                        client: client,
-                        secret: secret,
-                        authcode: authcode
-                    }
-                ]
-            }
-            sasJs.request('services/common/tokenauth', data).then(res => {
-                const generateTokenButton = document.querySelector('#generate-token');
-                generateTokenButton.style.display = 'none';
+    let tokenInfoP = document.querySelector('#token-info');
+    let tokenInfoMacroP = document.querySelector('#token-info-macro');
+    let tokenInfoEnvP = document.querySelector('#token-info-env');
 
-                let tokenInfo = JSON.stringify(res.tokeninfo[0], null, 2)
-                  .replace("ACCESS_TOKEN", "access_token")
-                  .replace("CLIENT", "client")
-                  .replace("SECRET", "secret")
-                  .replace("AUTHCODE", "authcode")
-                  .replace("REFRESH_TOKEN", "refresh_token");
+    tokenInfoP.innerHTML = tokenInfo;
+    tokenInfoP.style = '';
 
-                let tokenInfoEnv = `CLIENT=${res.tokeninfo[0].CLIENT}\nSECRET=${res.tokeninfo[0].SECRET}\nACCESS_TOKEN=${res.tokeninfo[0].ACCESS_TOKEN}\nREFRESH_TOKEN=${res.tokeninfo[0].REFRESH_TOKEN}`;
-
-                let access_token_macro = chunkString(res.tokeninfo[0].ACCESS_TOKEN, 240).join('%trim(\r\n)');
-                let refresh_token_macro = chunkString(res.tokeninfo[0].REFRESH_TOKEN, 240).join('%trim(\r\n)');
-
-                let tokenInfoMacro = `%let client=${res.tokeninfo[0].CLIENT};<br/>%let secret=${res.tokeninfo[0].SECRET};<br/>%let access_token=${access_token_macro};<br/>%let refresh_token=${refresh_token_macro};<br/>`;
-
-                let tokenInfoP = document.querySelector('#token-info');
-                let tokenInfoMacroP = document.querySelector('#token-info-macro');
-                let tokenInfoEnvP = document.querySelector('#token-info-env');
-
-                tokenInfoP.innerHTML = tokenInfo;
-                tokenInfoP.style = '';
-
-                tokenInfoMacroP.innerHTML = tokenInfoMacro;
-                tokenInfoEnvP.innerHTML = tokenInfoEnv;
-                let divs=document.getElementsByClassName('secHide')
-                function _removeClasses() {
-                  divs[0].classList.remove('secHide')
-                  if (divs[0]) _removeClasses()
-                }
-                _removeClasses()
-            })
-        });
-    });
+    tokenInfoMacroP.innerHTML = tokenInfoMacro;
+    tokenInfoEnvP.innerHTML = tokenInfoEnv;
+    let divs=document.getElementsByClassName('secHide')
+    function _removeClasses() {
+      divs[0].classList.remove('secHide')
+      if (divs[0]) _removeClasses()
+    }
+    _removeClasses()
+  })
 }
 
 function chunkString(str, length) {
